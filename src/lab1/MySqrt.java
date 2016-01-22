d75ac525240c62c256ce796171b1d27f172dde5b
Helsing <jonathan.helsing@gmail.com>
2016-01-22 13:59:40 +0100

    Första delen klar

--- src/lab1/MySqrt.java
+++ src/lab1/MySqrt.java
@@ -5,26 +5,38 @@ package lab1;
  * @version 0.5
  */
 public class MySqrt {
-
+    /**
+     * Finds the approximation to the squareroot of x with binary search.
+     *
+     * @param x The number to find the squareroot of.
+     * @param epsilon The margin of error. How close the approximation should be.
+     * @return An approximation of the squareroot of x.
+     */
     public static double mySqrtLoop(double x, double epsilon) {
-        if(x<0)
+        if(x < 0)
             return Double.NaN;
+
+        double min;
+        double max;
         if(x<1) {
-            double xOld = x;
-            double y = (1-x)/2.0;
-            double yOld;
-            while(!(Math.abs((y*y)-x) < epsilon)) {
-                yOld = y;
-                if(y*y > x) {
-                    y = (yOld - xOld) / 2.0;
-                } else if(y*y < x) {
-                    xOld = y;
-                    y = (1-yOld) / 2.0;
-                }
-            }
-            return y;
+            min = x;
+            max = 1;
+        } else {
+            //x>=1
+            min = 1;
+            max = x;
         }
-        return Double.NaN;
+        double y = (max+min)/2;
+
+        while(!(Math.abs((y*y)-x) < epsilon)) {
+            if((y*y) > x) {
+                max = y;
+            } else if ((y*y) < x) {
+                min = y;
+            }
+            y = (max+min)/2;
+        }
+        return y;
 
     }
 
@@ -33,7 +45,19 @@ public class MySqrt {
     }
 
     public static void main(String[] args) {
-        System.out.println("Svar: " + mySqrtLoop(0.25, 0.1));
+        System.out.println("Test of mySqrtLoop:");
+        System.out.println("Svar: " + mySqrtLoop(100, 0.000001));
+        System.out.println("Svar: " + mySqrtLoop(0, 0.000001));
+        System.out.println("Svar: " + mySqrtLoop(-20, 0.000001));
+        System.out.println("Svar: " + mySqrtLoop(0.25, 0.000001));
+        System.out.println("Svar: " + mySqrtLoop(1, 0.000001));
+
+        System.out.println("Test of mySqrtRecurse:");
+        System.out.println("Svar: " + mySqrtRecurse(100, 0.000001));
+        System.out.println("Svar: " + mySqrtRecurse(0, 0.000001));
+        System.out.println("Svar: " + mySqrtRecurse(-20, 0.000001));
+        System.out.println("Svar: " + mySqrtRecurse(0.25, 0.000001));
+        System.out.println("Svar: " + mySqrtRecurse(1, 0.000001));
     }
 
 }
