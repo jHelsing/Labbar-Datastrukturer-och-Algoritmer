3e354f27a85810ec2a7c7fab6d3eb75732f26b7f
Helsing <jonathan.helsing@gmail.com>
2016-03-04 20:43:29 +0100

    Lab 4 shortestPath done

--- src/lab4/DirectedGraph.java
+++ src/lab4/DirectedGraph.java
@@ -1,7 +1,9 @@
 package lab4;
 
+import java.awt.font.ImageGraphicAttribute;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.PriorityQueue;
 
 /**
  * @author Jonathan and Amar
@@ -18,6 +20,9 @@ public class DirectedGraph<E extends Edge> {
 
     public DirectedGraph(int noOfNodes) {
         edgeList = new ArrayList<>(noOfNodes);
+        for(int i=0; i<noOfNodes; i++) {
+            edgeList.add(new ArrayList<E>());
+        }
     }
 
     public void addEdge(E e) {
@@ -26,8 +31,49 @@ public class DirectedGraph<E extends Edge> {
     }
 
     public Iterator<E> shortestPath(int from, int to) {
-        // TODO använd Dijkstra's algorithm för detta
-        return null;
+        PriorityQueue<DijkstraObject> priorityQueue = new PriorityQueue<>(1, new CompDijkstraPath<DijkstraObject>());
+        priorityQueue.add(new DijkstraObject(from, new ArrayList<E>(), 0));
+
+        // edgeList has the same size as there are nodes in the graph
+        boolean[] isVisited = new boolean[edgeList.size()];
+
+        for(int i=0; i<isVisited.length; i++) {
+            isVisited[i] = false;
+        }
+
+        while(true) {
+            DijkstraObject queueHead = priorityQueue.poll();
+            while(queueHead != null && isVisited[queueHead.getPathEndNode()]) {
+                queueHead = priorityQueue.poll();
+            }
+
+
+            // Every node in the graph is not connected
+            if(queueHead == null)
+                return null;
+
+            // We have reached the specified end node
+            if (queueHead.getPathEndNode() == to)
+                return queueHead.getPath().iterator();
+
+            isVisited[queueHead.getPathEndNode()] = true;
+
+            ArrayList<E> edgesFromNode = edgeList.get(queueHead.getPathEndNode());
+
+            // Adds every edge that goes from the current node to the PriorityQueue
+            for(int i=0; i<edgesFromNode.size(); i++) {
+                E edge = edgesFromNode.get(i);
+                ArrayList<E> path = (ArrayList<E>) queueHead.getPath().clone();
+                path.add(edge);
+                DijkstraObject obj = new DijkstraObject(edge.getDest(), path, queueHead.getWeight() + edge.getWeight());
+                priorityQueue.add(obj);
+
+            }
+
+
+
+        }
+
     }
 
     public Iterator<E> minimumSpanningTree() {
@@ -40,18 +86,18 @@ public class DirectedGraph<E extends Edge> {
      */
     public class DijkstraObject {
 
-        private int endNode;
+        private int pathEndNode;
         private ArrayList<E> path;
         private double weight;
 
-        public DijkstraObject(int endNode, ArrayList<E> path, double weight) {
-            this.endNode = endNode;
+        public DijkstraObject(int pathEndNode, ArrayList<E> path, double weight) {
+            this.pathEndNode = pathEndNode;
             this.path = path;
             this.weight = weight;
         }
 
-        public int getEndNode() {
-            return endNode;
+        public int getPathEndNode() {
+            return pathEndNode;
         }
 
         public ArrayList<E> getPath() {
